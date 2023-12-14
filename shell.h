636701a04a7b4fdd3b3aad70f038b8963a261287
imeneHamida <imene.hamida18@gmail.com>
2023-12-13 18:08:05 -0800

    error fixed

--- shell.h
+++ shell.h
@@ -13,7 +13,7 @@ extern char **environ;
 void cmdline_t(char *token, const char *path, char **params);
 int empty_line(char *cmdline);
 void get_no_space_str(char **argv, char **params, char *token);
-
+void dir_not_exist(char *argv, char *dir);
 void error(char *msg, char *argv);
 void abs_path(char *relative_path, char *current_working_dir);
 void c_dir(char **params, char *argv);
@@ -21,7 +21,7 @@ void abs_path(char *relative_path, char *current_working_dir);
 char *_str_concat(char *s1, char *s2);
 void str_copy(char *dest, const char *src);
 int str_compare(const char *s1, const char *s2);
-int str_length(char *c);
+int strlen(char *c);
 int exit_sh(char **params, char *argv);
 int str_to_int(const char *str);
 char *path(char *argv);
